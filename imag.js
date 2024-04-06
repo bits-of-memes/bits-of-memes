514447e5c1db9f22c85e3c9ff02fa46f329542da
tejaswini-rb <114116509+tejaswini-rb@users.noreply.github.com>
2024-04-06 18:24:16 -0500

    fixed weird meme croppings

--- imag.js
+++ imag.js
@@ -2,4 +2,4 @@
 let images;
 
 export default images = ["./images/image0.jpg", "./images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.png", "images/image5.png", 
-    "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png", "images/image12.jpg", "images/image13.png", "images/image11.jpg", "images/cooking.jpeg"]
+    "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png", "images/image12.jpg", "images/image13.png", "images/image11.png", "images/cooking.jpeg"]

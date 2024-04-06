113b033287badf22fdbffbc09192f88f920166ff
tejaswini-rb <114116509+tejaswini-rb@users.noreply.github.com>
2024-04-06 10:30:49 -0500

    added draft website!

---
+++ script.js
@@ -0,0 +1,23 @@
+const catGifs = ["peachsit.gif", "gomaphone.gif", "peachboba.gif", "gomasilly.gif", "cooking.jpeg"];
+let currentCatIndex = 0;
+let buttonClicked = false;
+
+function toggleCatGif() {
+    currentCatIndex = (currentCatIndex + 1) % catGifs.length;
+    const gifContainer = document.getElementById('gif-container');
+    const catGif = document.getElementById('cat-gif');
+    catGif.src = catGifs[currentCatIndex];
+    gifContainer.classList.remove('hidden');
+
+    // Hide the hearts when the button is clicked
+    const hearts = document.querySelector('.hearts');
+    if (!buttonClicked) {
+        hearts.classList.add('hidden');
+        const buttonIcon = document.querySelector('.button-icon');
+        buttonIcon.textContent = 'click for more goofy meme :3';
+        buttonClicked = true;
+    }
+}
+
+// Event listener for the button
+document.querySelector('.button-icon').addEventListener('click', toggleCatGif);

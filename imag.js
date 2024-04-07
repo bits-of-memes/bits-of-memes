b0c7a327fb8833e776e262e5dc4c34840e4f154e
tejaswini-rb <114116509+tejaswini-rb@users.noreply.github.com>
2024-04-06 20:04:06 -0500

    fixed meme

--- imag.js
+++ imag.js
@@ -2,4 +2,5 @@
 let images;
 
 export default images = ["./images/image0.jpg", "./images/image1.jpg", "images/image2.jpg", "images/image3.jpg", "images/image4.png", "images/image5.png", 
-    "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png", "images/image12.jpg", "images/image13.png", "images/image11.png", "images/cooking.jpeg", "images/image15.png", "images/image14.png"]
+    "images/image6.png", "images/image7.png", "images/image8.png", "images/image9.png", "images/image10.png", "images/image12.jpg", "images/image13.png", 
+    "images/image11.png", "images/cooking.jpeg", "images/image15.png", "images/image14.png"]
